a7d0d2938d0de4ef89faff1ba07f08195c8ac7cb
brady.wang <brady.wang@qq.com>
2017-12-09 15:49:40 +0800

    071208 layer learn

--- ui/1.php
+++ ui/1.php
@@ -12,17 +12,9 @@
 <style>
 
 </style>
-
-<pre>
-    <div>
-        <ul>
-            <li>test</li>
-        </ul>
-    </div>
-<ul>
-    <li>HTML将不会被解析</li>
-    <li>有木有感觉非常方便</li>
-</ul>
+<button id="button1" class="layui-button layui-button-normal ">
+    点击提交
+</button>
 <script>
     !function(){
         var a = 123;
@@ -37,6 +29,51 @@
         encode:true,
         about:false
     })
+    layui.use(['layer'],function(){
+        var layer = layui.layer;
+        $("#button1").click(function(){
+
+            layer.ready(function(){
+                layer.open({
+                    type:1,
+                    content: $("#content"),
+                    success:function(layero,index){
+
+                    }
+                    ,btn: ['确定', '取消']
+                    ,yes: function(index, layero){
+                        layer.msg("确定")
+                        layer.close(index);
+                    }
+                    ,btn2: function(index, layero){
+                        //按钮【按钮二】的回调
+                        layer.msg("取消");
+                        layer.close(index);
+                    }
+                });
+            })
+        })
+
+    })
+
+    parent.document.onkeydown = function(e){
+        var ev =parent. document.all ? window.event : e;
+        if(ev.keyCode==13) {
+            $('#loginBtn').trigger("click");
+            return false
+        }
+    }
+    document.onkeydown = function(e){
+        var ev =document.all ? window.event : e;
+        if(ev.keyCode==13) {
+            $('#loginBtn').trigger("click");
+            return false
+        }
+    }
 </script>
 </body>
 </html>
+
+<div id="content" style="display:none;">
+
+</div>
