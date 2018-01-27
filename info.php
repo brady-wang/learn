2d10ed29b50a32c9c43528c4ae7e9cdc0cbfcf1e
brady.wang <brady.wang@qq.com>
2018-01-27 17:26:48 +0800

    e

---
+++ info.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brady
+ * Date: 2018/1/27
+ * Time: 15:39
+ */
+phpinfo();
---
+++ python/test_python.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brady
+ * Date: 2018/1/27
+ * Time: 14:10
+ */
+$data = $_POST;
+echo json_encode($_SERVER);
