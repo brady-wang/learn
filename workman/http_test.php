14de29e17ba14f0af8e51e630140c551bd3eac2c
brady.wang <brady.wang@qq.com>
2018-01-31 18:05:46 +0800

    444

---
+++ workman/http_test.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brady
+ * Date: 2018/1/31
+ * Time: 16:46
+ */
+use Workerman\Worker;
+require_once __DIR__ . '/Autoloader.php';
+// 创建一个Worker监听2345端口，使用http协议通讯
+$http_worker = new Worker("http://192.168.33.30:2345");
+
+// 启动4个进程对外提供服务
+$http_worker->count = 4;
+
+// 接收到浏览器发送的数据时回复hello world给浏览器
+$http_worker->onMessage = function($connection, $data)
+{
+    // 向浏览器发送hello world
+    $connection->send('接收到浏览器数据:'.json_encode($data));
+};
+
+// 运行worker
+Worker::runAll();
